d708e3ef95ef3ffca6fb6ad132b8dde9687d6c60
Kalk <simon.d.mansson@gmail.com>
2021-03-23 23:25:49 +0100

    add file watcher script

---
+++ watcher.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+# watch file(s) given by arg1 and run command from arg2 on event modification.
+FOLDER=${1}
+COMMAND=${2}
+echo  Watching ${FOLDER}, running ${COMMAND} on changes...
+inotifywait -e modify -r -m ${FOLDER} |
+while read dir event file; do
+        # dir event file is the format of inotifywait events. but we only watch
+        # we only watch for modify
+        # create a subshell and run the command from the script callers location
+        $(cd $(pwd) && echo "${COMMAND}")
+done
+
